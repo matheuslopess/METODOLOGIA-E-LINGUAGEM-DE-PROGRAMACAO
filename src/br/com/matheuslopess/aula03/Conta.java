b418cd0b0a1b0f3657b4ed814f591a604dcfa947
Matheus <34162888+matheuslopess@users.noreply.github.com>
2018-08-22 21:25:57 -0300

    Conta de Banco da Aula 03 inserida

---
+++ src/br/com/matheuslopess/aula03/Conta.java
@@ -0,0 +1,40 @@
+package br.com.matheuslopess.aula03;
+
+public class Conta {
+	private String nome;
+	private double saldo;
+	private long numeroConta;
+	
+	public double getSaldo(long numeroConta) {
+		if(this.numeroConta == numeroConta)
+			return this.saldo;
+		
+		return 0.0;
+	}
+	
+	public String getDados() {
+		String dados = "Nome: " + this.nome;
+		dados += "Numero: " + this.numeroConta;
+		dados += "Saldo: " + this.saldo;
+		
+		return dados;
+	}
+	
+	public boolean depositar(double valor) {
+		if(valor < 0)
+			return false;
+		else
+			this.saldo += valor;
+		
+		return true;
+	}
+	
+	public boolean sacar(double valor) {
+		if(valor < 0 || valor > this.saldo)
+			return false;
+		else
+			this.saldo -= valor;
+		
+		return true;	
+	}
+}

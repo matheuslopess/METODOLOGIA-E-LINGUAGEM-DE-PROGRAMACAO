2cdf0d72fcef7555d6670c7ca8f7de3dd509cc76
matheus <matheus@matheus>
2018-10-04 12:52:06 -0300

    Finalização da aula 04

--- src/br/com/matheuslopess/aula04/exercicios/Calculadora.java
+++ src/br/com/matheuslopess/aula04/exercicios/Calculadora.java
@@ -1,19 +1,20 @@
 package br.com.matheuslopess.aula04.exercicios;
 
 public class Calculadora {
-	static double somar(double a, double b) {
+	
+	public static double somar(double a, double b) {
 		return a + b;
 	}
 	
-	static double subtrair(double a, double b) {
+	public static double subtrair(double a, double b) {
 		return a - b;
 	}
 	
-	static double multiplicar(double a, double b) {
+	public static double multiplicar(double a, double b) {
 		return a * b;
 	}
 	
-	static double dividir(double a, double b) {
+	public static double dividir(double a, double b) {
 		return a / b;
 	}
 }
